2e91e2883a1d2f8d575ecbce1870f47eba9e3546
haizenburg <tshepangmoima87@gmail.com>
2022-07-17 15:12:21 +0200

    init comm

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const AutoComplete = ({ suggestions }: any) => {
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onChange = (e: any) => {
+    const userInput = e.target.value;
+
+    // Filter our suggestions that don't contain the user's input
+    const unLinked = suggestions.filter(
+      (suggestion: any) =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    setInput(e.target.value);
+    setFilteredSuggestions(unLinked);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(true);
+  };
+
+  const onClick = (e: any) => {
+    setFilteredSuggestions([]);
+    setInput(e.target.innerText);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(false);
+  };
+  const SuggestionsListComponent = () => {
+    return filteredSuggestions.length ? (
+      <ul className="suggestions">
+        {filteredSuggestions.map((suggestion, index) => {
+          let className;
+          // Flag the active suggestion with a class
+          if (index === activeSuggestionIndex) {
+            className = "suggestion-active";
+          }
+          return (
+            <li className={className} key={suggestion} onClick={onClick}>
+              {suggestion}
+            </li>
+          );
+        })}
+      </ul>
+    ) : (
+      <div className="no-suggestions">
+        <em>No suggestions, you're on your own!</em>
+      </div>
+    );
+  };
+  const onKeyDown = (key: any) => {
+    if (key.keyCode === 13 || key.keyCode === 9) {
+      setInput(filteredSuggestions[activeSuggestionIndex]);
+      setFilteredSuggestions([]);
+    }
+  };
+  return (
+    <>
+      <input
+        type="text"
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      {showSuggestions && input && <SuggestionsListComponent />}
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoComplete
+        suggestions={[
+          "Alligator",
+          "Bask",
+          "Crocodilian",
+          "Death Roll",
+          "Eggs",
+          "Jaws",
+          "Reptile",
+          "Solitary",
+          "Tail",
+          "Wetlands",
+        ]}
+      />
     </div>
   );
 }
